9a56aabd220cd8ffa0be39eb05a7c644c1194880
peiyaow <monicawang76@gmail.com>
2018-08-31 15:09:36 -0400

    change

--- exampleTVGL.py
+++ exampleTVGL.py
@@ -1,6 +1,10 @@
 import TVGL as tvgl
 import numpy as np 
 
+import sys
+import os
+sys.path.append(os.path.abspath('..')+'/snap')
+
 Cov = np.array([[5, 1], [1, 7]])
 data = np.random.multivariate_normal(np.zeros(2), Cov, 50)
 
@@ -10,7 +14,7 @@ data = data[0:30,0:10]
 lamb = 2.5
 beta = 12
 lengthOfSlice = 10
-thetaSet = tvgl.TVGL(data, lengthOfSlice, lamb, beta, indexOfPenalty = 4, verbose=True)
+thetaSet = tvgl.TVGL(data, lengthOfSlice, lamb, beta, indexOfPenalty = 1, verbose=True)
 print thetaSet
 
 
